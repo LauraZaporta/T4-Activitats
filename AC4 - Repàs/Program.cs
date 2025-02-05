b0cfb0764fce0f471716a05e990effe9401d9773
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-02-05 11:53:29 +0100

    Exercici 4 upload

--- AC4 - Repàs/Program.cs
+++ AC4 - Repàs/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace AC4R
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // EXERCICI 4
+            int numElements = 0;
+
+            List<string> names = new List<string>();
+            Console.Write("Quants elements vols guardar? ");
+            numElements = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numElements; i++)
+            {
+                Console.Write("Escriu valor: ");
+                names.Add(Console.ReadLine());
+            }
+            names.Sort();
+            foreach (var element in names)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
